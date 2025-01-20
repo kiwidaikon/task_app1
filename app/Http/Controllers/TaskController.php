e600dc2440434f3b253964ea735906a7a73b5d9f
kiwidaikon <niratamagomoti@yahoo.co.jp>
2025-01-20 11:09:06 +0900

    view/tasks/show.blade.php作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    // showページへ移動
+    public function show($id)
+    {
+        $task = task::find($id);
+        return view('tasks.show', ['task' => $task]);
+    }
 }
