1aacf7119db7e429060e51ebe4a3898973d4095d
kiwidaikon <niratamagomoti@yahoo.co.jp>
2025-01-20 11:31:47 +0900

    タスク登録追加

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,29 @@
+ <!DOCTYPE html>
+ <html lang="ja">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>task create</title>
+     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+ </head>
+
+ <body>
+     <a href="{{ route('tasks.index') }}">戻る</a>
+     <h1>タスク登録</h1>
+     <form action="{{ route('tasks.store') }}" method="post">
+     @csrf
+         <p>
+             <label for="title">タイトル</label><br>
+             <input type="text" name="title" id="title">
+         </p>
+         <p>
+             <label for="body">本文</label><br>
+             <textarea name="body" class="body" id="body"></textarea>
+         </p>
+
+         <input type="submit" value="登録">
+     </form>
+ </body>
+
+ </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -15,6 +15,9 @@
              <li><a href="{{ route('tasks.show', $task) }}">{{ $task->title }}</a></li>
          @endforeach
      </ul>
+
+     <!-- 新規登録画面へジャンプする -->
+     <button onclick='location.href="{{ route('tasks.create') }}"'>登録する</button>
  </body>
 
  </html>
